b440c19e849c8706b8a77567ddb28195eb7d32f5
Kyle Hasty <khasty@tenable.com>
2021-10-16 10:52:16 -0400

    Init commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import QRCode from "react-qr-code";
 
 export default function App() {
+  const [code, onInputChange] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <QRCode 
+        value={code || 'www.exmaple.com'} 
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onInputChange}
+        placeholder="www.exmaple.com"
+        value={code}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +27,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
